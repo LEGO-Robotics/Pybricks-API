f5ed550f070d3fe2945c874c02a363d9672d4afb
The Vinh Luong (Lương Thế Vinh) <TheVinhLuong@gmail.com>
2022-09-03 13:25:21 -0700

    ABM-decorate EV3Speaker in src.pybricks.abm submodule

--- src/pybricks/abm.py
+++ src/pybricks/abm.py
@@ -13,6 +13,7 @@ from .iodevices import (AnalogSensor,   # noqa: F401
                         LWP3Device,
                         PUPDevice,
                         UARTDevice)
+from .ev3dev._speaker import Speaker as EV3Speaker
 from .robotics import DriveBase
 from .tools import StopWatch
 from . import tools
@@ -31,6 +32,30 @@ def decor(cls: type, method_name: str, decorator: callable, /):
 # (pybricks.com/ev3-micropython/hubs#pybricks.hubs.EV3Brick)
 # ----------------------------------------------------------
 # TODO
+
+# EV3Speaker
+# ----------
+for a in (
+    # pybricks.com/ev3-micropython/hubs#pybricks.hubs.EV3Brick.speaker.beep
+    'beep',
+
+    # pybricks.com/ev3-micropython/hubs#pybricks.hubs.EV3Brick.speaker.play_notes
+    'play_notes',
+
+    # pybricks.com/ev3-micropython/hubs#pybricks.hubs.EV3Brick.speaker.play_file
+    'play_file',
+
+    # pybricks.com/ev3-micropython/hubs#pybricks.hubs.EV3Brick.speaker.say
+    'say',
+
+    # pybricks.com/ev3-micropython/hubs#pybricks.hubs.EV3Brick.speaker.set_speech_options
+    'set_speech_options',
+
+    # pybricks.com/ev3-micropython/hubs#pybricks.hubs.EV3Brick.speaker.set_volume
+    'set_volume',
+):
+    decor(EV3Speaker, a, act)
+
 
 # InventorHub
 # (docs.pybricks.com/en/latest/hubs/primehub.html#InventorHub)
