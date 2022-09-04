277ecbfadaaa6f9c39b73c9d15175c3c931c06af
The Vinh Luong (Lương Thế Vinh) <TheVinhLuong@gmail.com>
2022-09-04 15:59:48 -0700

    ABM-decorate pybricks.iodevices in pybricks.abm.pup submodule

---
+++ src/pybricks/abm/pup/__init__.py
@@ -0,0 +1,17 @@
+"""ABM-decorating pybricks.iodevices."""
+
+
+# iodevices
+# (docs.pybricks.com/en/latest/pupdevices)
+# ========================================
+from . import (hubs,   # noqa: F401
+               motor,
+               color_light_matrix,
+               color_sensor,
+               color_distance_sensor,
+               force_sensor,
+               infrared_sensor,
+               light,
+               remote,
+               tilt_sensor,
+               ultrasonic_sensor)
---
+++ src/pybricks/abm/pup/hubs.py
@@ -0,0 +1,34 @@
+"""ABM-decorating Powered-Up Hubs."""
+
+
+from ...hubs import InventorHub, PrimeHub, CityHub, MoveHub, TechnicHub   # noqa: F401
+
+
+# hubs
+# (docs.pybricks.com/en/latest/hubs)
+# ==================================
+
+# InventorHub
+# (docs.pybricks.com/en/latest/hubs/primehub.html#InventorHub)
+# ------------------------------------------------------------
+# TODO
+
+# PrimeHub
+# (docs.pybricks.com/en/latest/hubs/primehub.html#pybricks.hubs.PrimeHub)
+# -----------------------------------------------------------------------
+# TODO
+
+# CityHub
+# (docs.pybricks.com/en/latest/hubs/cityhub.html#pybricks.hubs.CityHub)
+# ---------------------------------------------------------------------
+# TODO
+
+# MoveHub
+# (docs.pybricks.com/en/latest/hubs/movehub.html#pybricks.hubs.MoveHub)
+# ---------------------------------------------------------------------
+# TODO
+
+# TechnicHub
+# (docs.pybricks.com/en/latest/hubs/technichub.html#pybricks.hubs.TechnicHub)
+# ---------------------------------------------------------------------------
+# TODO
