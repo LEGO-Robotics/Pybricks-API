d02f260d8d441083550640d16a76f08ec02bc79a
The Vinh Luong (Lương Thế Vinh) <TheVinhLuong@gmail.com>
2022-09-04 20:25:17 -0700

    skip decorating already-decorated methods in src.pybricks.abm.pup.motor

--- src/pybricks/abm/pup/motor/__init__.py
+++ src/pybricks/abm/pup/motor/__init__.py
@@ -11,48 +11,48 @@ from . import dc, pf   # noqa: F401
 # -----------------------------------------------------------------------------
 for a in (
     # docs.pybricks.com/en/latest/pupdevices/motor.html#pybricks.pupdevices.Motor.reset_angle
-    'reset_angle',
+    # 'reset_angle',   # already decorated in _common.Motor
 
     # docs.pybricks.com/en/latest/pupdevices/motor.html#pybricks.pupdevices.Motor.stop
-    'stop',
+    # 'stop',   # already decorated in _common.DCMotor
 
     # docs.pybricks.com/en/latest/pupdevices/motor.html#pybricks.pupdevices.Motor.brake
-    'brake',
+    # 'brake',   # already decorated in _common.DCMotor
 
     # docs.pybricks.com/en/latest/pupdevices/motor.html#pybricks.pupdevices.Motor.hold
-    'hold',
+    # 'hold',   # already decorated in _common.Motor
 
     # docs.pybricks.com/en/latest/pupdevices/motor.html#pybricks.pupdevices.Motor.run
-    'run',
+    # 'run',   # already decorated in _common.Motor
 
     # docs.pybricks.com/en/latest/pupdevices/motor.html#pybricks.pupdevices.Motor.run_time
-    'run_time',
+    # 'run_time',   # already decorated in _common.Motor
 
     # docs.pybricks.com/en/latest/pupdevices/motor.html#pybricks.pupdevices.Motor.run_angle
-    'run_angle',
+    # 'run_angle',   # already decorated in _common.Motor
 
     # docs.pybricks.com/en/latest/pupdevices/motor.html#pybricks.pupdevices.Motor.run_target
-    'run_target',
+    # 'run_target',   # already decorated in _common.Motor
 
     # docs.pybricks.com/en/latest/pupdevices/motor.html#pybricks.pupdevices.Motor.track_target
-    'track_target',
+    # 'track_target',   # already decorated in _common.Motor
 
     # docs.pybricks.com/en/latest/pupdevices/motor.html#pybricks.pupdevices.Motor.run_until_stalled
-    'run_until_stalled',
+    # 'run_until_stalled',   # already decorated in _common.Motor
 
     # docs.pybricks.com/en/latest/pupdevices/motor.html#pybricks.pupdevices.Motor.dc
-    'dc',
+    # 'dc',   # already decorated in _common.DCMotor
 
     # docs.pybricks.com/en/latest/pupdevices/motor.html#pybricks.pupdevices.Motor.settings
-    'settings',   # *** NOTE: OVERLOADED METHOD ***
+    # 'settings',   # already decorated in _common.DCMotor
 ):
     act_decor(Motor, a)
 
 for s in (
     # docs.pybricks.com/en/latest/pupdevices/motor.html#pybricks.pupdevices.Motor.speed
-    'speed',
+    # 'speed',   # already decorated in _common.Motor
 
     # docs.pybricks.com/en/latest/pupdevices/motor.html#pybricks.pupdevices.Motor.angle
-    'angle',
+    # 'angle',   # already decorated in _common.Motor
 ):
     sense_decor(Motor, s)
