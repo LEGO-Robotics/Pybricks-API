4ccbb24e9ace9de258e7cba2642e0e1cf40064a1
The Vinh Luong (Lương Thế Vinh) <TheVinhLuong@gmail.com>
2022-09-04 20:44:19 -0700

    skip decorating already-decorated methods in src.pybricks.abm.pup.color_sensor.distance submodule

--- src/pybricks/abm/pup/color_sensor/distance.py
+++ src/pybricks/abm/pup/color_sensor/distance.py
@@ -10,24 +10,24 @@ from ...util import act_decor, sense_decor
 # ---------------------------------------------------------------------------------------------------------
 for a in (
     # docs.pybricks.com/en/latest/pupdevices/colordistancesensor.html#pybricks.pupdevices.ColorDistanceSensor.detectable_colors
-    'detectable_colors',   # *** NOTE: OVERLOADED METHOD ***
+    # 'detectable_colors',   # already decorated in _common.CommonColorSensor
 ):
     act_decor(ColorDistanceSensor, a)
 
 for s in (
     # docs.pybricks.com/en/latest/pupdevices/colordistancesensor.html#pybricks.pupdevices.ColorDistanceSensor.color
-    'color',
+    # 'color',   # already decorated in _common.CommonColorSensor
 
     # docs.pybricks.com/en/latest/pupdevices/colordistancesensor.html#pybricks.pupdevices.ColorDistanceSensor.reflection
-    'reflection',
+    # 'reflection',   # already decorated in _common.CommonColorSensor
 
     # docs.pybricks.com/en/latest/pupdevices/colordistancesensor.html#pybricks.pupdevices.ColorDistanceSensor.ambient
-    'ambient',
+    # 'ambient',   # already decorated in _common.CommonColorSensor
 
     # docs.pybricks.com/en/latest/pupdevices/colordistancesensor.html#pybricks.pupdevices.ColorDistanceSensor.distance
     'distance',
 
     # docs.pybricks.com/en/latest/pupdevices/colordistancesensor.html#pybricks.pupdevices.ColorDistanceSensor.hsv
-    'hsv',
+    # 'hsv',   # already decorated in _common.CommonColorSensor
 ):
     sense_decor(ColorDistanceSensor, s)
