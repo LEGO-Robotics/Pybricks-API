7d2b5463910d445e413ea1c6c20ada9a059d3210
The Vinh Luong (Lương Thế Vinh) <TheVinhLuong@gmail.com>
2022-09-04 20:16:39 -0700

    skip decorating already-decorated methods in src.pybricks.abm.nxt.color_sensor

--- src/pybricks/abm/nxt/color_sensor.py
+++ src/pybricks/abm/nxt/color_sensor.py
@@ -10,13 +10,13 @@ from ..util import sense_decor
 # -------------------------------------------------------------------------
 for s in (
     # pybricks.com/ev3-micropython/nxtdevices#pybricks.nxtdevices.ColorSensor.color
-    'color',
+    # 'color',   # already decorated in _common.CommonColorSensor
 
     # pybricks.com/ev3-micropython/nxtdevices#pybricks.nxtdevices.ColorSensor.ambient
-    'ambient',
+    # 'ambient',   # already decorated in _common.CommonColorSensor
 
     # pybricks.com/ev3-micropython/nxtdevices#pybricks.nxtdevices.ColorSensor.reflection
-    'reflection',
+    # 'reflection',   # already decorated in _common.CommonColorSensor
 
     # pybricks.com/ev3-micropython/nxtdevices#pybricks.nxtdevices.ColorSensor.rgb
     'rgb',
